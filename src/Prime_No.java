6b8aeeb2194f17afb5591690a3b828df83b09ee5
GauravShripad <gauravs.csea19@sbjit.edu.in>
2023-09-20 09:07:26 +0530

    prime_number

--- src/Prime_No.java
+++ src/Prime_No.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Prime_No {
+public class Prime_No {
+    public static void main(String[] args){
+        int count = 50;
+        System.out.print("2 3 5 ");
+        for(int i = 6; i < count; i++){
+            if(i % 2 == 0 || i % 3==0 || i % 5 ==0){
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
